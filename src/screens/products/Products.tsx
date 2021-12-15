d2025c627caad796363a822f1ea678b8daf07e9e
Adrián <adrianbreyb@gmai.com>
2021-12-15 09:51:32 +0100

    State + linear progress
    añado un state para almacenar los productos del JSON y una animacion de carga mientras se obtienen los productos

--- src/screens/products/Products.tsx
+++ src/screens/products/Products.tsx
@@ -1,13 +1,15 @@
 import { useNavigation } from '@react-navigation/native';
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { SafeAreaView } from 'react-native';
-import { Button, Text } from 'react-native-elements';
+import { Button, LinearProgress, Text } from 'react-native-elements';
 import Counter from '../../app/components/counter/Counter';
 import ProductsService from '../../app/services/ProductsService';
 import styles from './product-styles';
 
 const Products = () => {
+  const [Products, setProducts] = useState([]);
   const navigation = useNavigation<any>();
+
 
   const onPressButton = () => {
     navigation.navigate('product-details');
@@ -17,13 +19,18 @@ const Products = () => {
 
   useEffect(() => {
     const asyncCall = async () => {
-      console.log('calling');
       const result = await ProductsService.getProducts();
-      console.log(result.data);
+      setProducts(result.data);
     }
 
     asyncCall();
   }, []);
+
+  console.log('products', Products);
+
+  if (Products.length === 0) {
+    return <LinearProgress />;
+  }
 
   return (
     <SafeAreaView style={styles.container}>
